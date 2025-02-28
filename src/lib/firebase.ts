218584dad09b757b5352748547421b239b5e1b8f
Kalana Weerarathna <kalana.weerarathna@circles.co>
2025-02-28 11:43:31 +0530

    update firebase details

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { initializeFirestore, CACHE_SIZE_UNLIMITED, persistentLocalCache } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id"
+  projectId: 'a-pos-10203',
+  appId: '1:1071999236972:web:aeec4384d2b22f9dd31149',
+  storageBucket: 'a-pos-10203.appspot.com',
+  apiKey: 'AIzaSyCSOcP-2WfphlWaSsGk8FWrZxzImFG3wFk',
+  authDomain: 'a-pos-10203.firebaseapp.com',
+  messagingSenderId: '1071999236972',
+  measurementId: 'G-DH2RM4G560',
 };
 
 // Initialize Firebase
@@ -18,5 +19,3 @@ const app = initializeApp(firebaseConfig);
 export const db = initializeFirestore(app, {
   localCache: persistentLocalCache({cacheSizeBytes: CACHE_SIZE_UNLIMITED})
 });
-
-export default db;
